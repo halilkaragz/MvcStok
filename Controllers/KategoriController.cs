6dc76d8cca19c93e606c7041f4ce40684133107d
halilkaragz <halilkaragoz_blsm@hotmail.com>
2022-07-29 08:05:18 +0300

    Add project files.

---
+++ Controllers/KategoriController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        dbMvcStokEntities db = new dbMvcStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.Kategoriler.ToList();
+            return View(degerler);
+        }
+
+    }
+}
---
+++ Controllers/MusteriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        dbMvcStokEntities db = new dbMvcStokEntities();
+        public ActionResult Index()
+        {
+            var musteriler = db.Musteriler.ToList();
+            return View(musteriler);
+        }
+    }
+}
